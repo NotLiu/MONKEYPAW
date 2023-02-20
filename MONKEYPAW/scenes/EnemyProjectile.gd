df864be484ec6cd8c33d24b15463a234872aa6b5
Anna Zheng <26550446+azheng9096@users.noreply.github.com>
2023-02-20 00:41:49 -0500

    lifetime for projectile

--- MONKEYPAW/scenes/EnemyProjectile.gd
+++ MONKEYPAW/scenes/EnemyProjectile.gd
@@ -4,10 +4,14 @@ extends KinematicBody2D
 # Declare member variables here. Examples:
 var direction : Vector2 = Vector2.LEFT
 var speed : float = 250
+var lifetime : float = 20
+
+onready var timer = $Timer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	timer.connect("timeout", self, "queue_free")
+	timer.start(lifetime)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
